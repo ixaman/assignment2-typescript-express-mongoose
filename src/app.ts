9fbea914a0da165ead540ccd33cb848d23ee329c
ixaman <iasadxaman@gmail.com>
2023-11-27 03:00:01 +0600

    env, db project setup done

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+
+const app: Application = express()
+
+// parser
+app.use(express.json())
+app.use(express.text())
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+export default app
